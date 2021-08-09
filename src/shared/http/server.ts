b369fe7f161a1dda0eb42422b01cf3f5a76af473
psbrunosouza <psbrunosouza@gmail.com>
2021-08-09 18:58:22 -0300

    initial commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,31 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      response.status(error.status).json({
+        status: 'error',
+        message: error.message,
+      });
+    } else {
+      response.status(500).json({
+        status: 'error',
+        message: 'internal server error',
+      });
+    }
+  },
+);
+
+app.listen(3333, () => {
+  console.log('[API] Server started on port 3333');
+});
