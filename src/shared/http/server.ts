1af0b93e0f6ee42153b7a09b88e6f5e6aeb7065a
psbrunosouza <psbrunosouza@gmail.com>
2021-08-12 22:01:06 -0300

    type orm configs

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/connection';
 
 const app = express();
 
@@ -27,5 +29,5 @@ app.use(
 );
 
 app.listen(3333, () => {
-  console.log('[API] Server started on port 3333');
+  console.log('[API] Server started');
 });
